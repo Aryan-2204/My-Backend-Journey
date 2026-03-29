d8ae9ccfe87371bcd7814e3240bdebfe15a6c8d9
Aryxnnn <akadam2204@gmail.com>
2026-03-29 18:49:22 +0530

    response

--- First_Node_Server/app.js
+++ First_Node_Server/app.js
@@ -3,6 +3,29 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url,req.method,req.headers);
+   
+    if (req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Node Server</title></head>');
+    res.write('<body><h1>Welcome to Home</h1></body>');
+    res.write('</html>');
+    return res.end();
+
+    } else if (req.url === '/products'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Node Server</title></head>');
+    res.write('<body><h1>Products</h1></body>');
+    res.write('</html>');
+    return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Node Server</title></head>');
+    res.write('<body><h1>Hello, World!</h1></body>');
+    res.write('</html>');
+    res.end();
     //process.exit() is used to stop the server after the first request is received. This is just for demonstration purposes. In a real application, you would typically not want to stop the server after handling a request.
     
 });
